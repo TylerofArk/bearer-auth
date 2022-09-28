8c82d8e9df035e2b5a4a6126465183c832f21729
Tyler Main <tyler.l.main@gmail.com>
2022-09-27 23:28:13 -0500

    debug attempt 1

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -5,7 +5,9 @@ const { user } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
 
-  if (!req.headers.authorization) { return _authError(); }
+  if (!req.headers.authorization) {
+    next('invalid login');
+  }
 
   let basic = req.headers.authorization;
   let [username, pass] = base64.decode(basic).split(':');
