b7028aebe6cb23278bdf67b279a64c61c2557cda
Tyler Main <tyler.l.main@gmail.com>
2022-09-27 22:36:57 -0500

    starter code completed

--- src/auth/router/handlers.js
+++ src/auth/router/handlers.js
@@ -7,7 +7,7 @@ async function handleSignup(req, res, next) {
     let userRecord = await users.create(req.body);
     const output = {
       user: userRecord,
-      token: userRecord.token
+      token: userRecord.token,
     };
     res.status(200).json(output);
   } catch (e) {
@@ -20,7 +20,7 @@ async function handleSignin(req, res, next) {
   try {
     const user = {
       user: request.user,
-      token: request.user.token
+      token: request.user.token,
     };
     res.status(200).json(user);
   } catch (e) {
@@ -48,5 +48,5 @@ module.exports = {
   handleSignup,
   handleSignin,
   handleGetUsers,
-  handleSecret
-}
+  handleSecret,
+};
